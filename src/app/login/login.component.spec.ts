0cc7f2a74ea87aaad4e29443b886876c5a10f6fb
kesari_vikas <vikaskumar.kesari@citiustech.com>
2018-08-28 20:22:19 +0530

    Styling and login unit testing.

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { Login } from './login';
 import { LoginService } from './login.service';
+import {Routes, RouterModule} from '@angular/router';
 //import {By} from "@angular/platform-browser";
 //import { FormGroup, FormBuilder } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
