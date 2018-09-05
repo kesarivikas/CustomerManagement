a0d9aba05ff5b009a20ef42a9b852996afcf8ae4
kesari_vikas <vikaskumar.kesari@citiustech.com>
2018-09-05 16:16:15 +0530

    removed static file

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/CustomerManagement'));
+app.use(express.static('dist'));
 
 app.get('/*', function(req, res) { 
-	res.sendFile(path.join(__dirname + '/dist/CustomerManagement/index.html'));
+	res.sendFile('index.html');
 });
 
 // Start the app by listening on the default Heroku port
