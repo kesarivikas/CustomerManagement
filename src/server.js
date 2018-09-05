4bd28ac71d8556eaeaaf86b6f9bae5908add9c89
kesari_vikas <vikaskumar.kesari@citiustech.com>
2018-09-05 15:16:49 +0530

    Changes server file.

--- src/server.js
+++ src/server.js
@@ -2,22 +2,17 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const http = require('http');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname, 'dist'));
+app.use(express.static(__dirname, '/dist'));
 
-app.get('*', function(req, res) {
-	res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.get('*', function(req, res) { 
+	res.sendFile(__dirname, '/dist/index.html');
 });
 
 // Start the app by listening on the default Heroku port
-const port = process.env.PORT || '8080';
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, () => console.log('app is running....'));
+app.listen(process.env.PORT || 8080);
 
 console.log('server listening......');
