7d26817edfdff5912593e22b9c4e769226482f31
kesari_vikas <vikaskumar.kesari@citiustech.com>
2018-09-05 14:50:32 +0530

    Created server.

--- src/server.js
+++ src/server.js
@@ -2,17 +2,22 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const http = require('http');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname, 'dist'));
 
-app.get('*', function(req, res) { 
-	res.sendFile(path.join(__dirname, 'dist','index.html'));
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('app is running....'));
 
 console.log('server listening......');
