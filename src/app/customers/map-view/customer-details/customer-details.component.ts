7ecb8dff4d8e50046b22da51fad92910d75fb546
kesari_vikas <vikaskumar.kesari@citiustech.com>
2018-09-11 13:11:05 +0530

    Updated Order json.

--- src/app/customers/map-view/customer-details/customer-details.component.ts
+++ src/app/customers/map-view/customer-details/customer-details.component.ts
@@ -13,9 +13,11 @@ export class CustomerDetailsComponent implements OnInit {
   constructor(private activatedRoutes: ActivatedRoute, private customerService: CustomersService) { }
 	public firstName: string;
 	public currentCustomer: Customers;
+	public zoom: number;
 	// lat: number = 51.678418;
   // lng: number = 7.809007;
   ngOnInit() {
+//		this.zoom = 15;
 		this.firstName = this.activatedRoutes.snapshot.queryParamMap.get('firstName');
 		// this.customerService.setCustomer(this.firstName);
 		this.customerService.getCustomersList().forEach(customer => {
